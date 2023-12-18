b4e3f27459be0f55f72d8bbb5e47145ff045d6b9
crendyyy <crendy.realdi12@gmail.com>
2023-12-18 08:42:43 +0700

    some update

--- resources/views/components/layout/new-product.blade.php
+++ resources/views/components/layout/new-product.blade.php
@@ -1,11 +1,11 @@
-<div class="flex  flex-col gap-4 py-6 rounded-lg">
+<div class="flex flex-col gap-4 py-6 rounded-lg">
     <div class="flex justify-between">
         <span class="text-2xl font-bold">Produk Terbaru</span>
         <a href="" class="font-semibold text-primary">Lihat Semua</a>
     </div>
     <div class="flex flex-col gap-6">
         <div class="flex justify-start w-full gap-4">
-            <div class="flex flex-col w-1/4 bg-white rounded-2xl">
+            <div class="flex flex-col w-[calc(100%/6)] bg-white rounded-2xl">
                 <img class="w-full bg-gray-300 h-60 rounded-t-2xl">
                 <div class="flex flex-col w-full gap-4 p-4 bg-white rounded-b-2xl">
                     <div class="flex flex-col justify-start gap-2">
@@ -17,7 +17,7 @@
                     <span class="text-sm text-gray-400">Outwear</span>
                 </div>
             </div>
-            <div class="flex flex-col w-1/4 bg-white rounded-2xl">
+            <div class="flex flex-col w-[calc(100%/6)] bg-white rounded-2xl">
                 <img class="w-full bg-gray-300 h-60 rounded-t-2xl">
                 <div class="flex flex-col w-full gap-4 p-4 bg-white rounded-b-2xl">
                     <div class="flex flex-col justify-start gap-2">
@@ -29,7 +29,7 @@
                     <span class="text-sm text-gray-400">Outwear</span>
                 </div>
             </div>
-            <div class="flex flex-col w-1/4 bg-white rounded-2xl">
+            <div class="flex flex-col w-[calc(100%/6)] bg-white rounded-2xl">
                 <img class="w-full bg-gray-300 h-60 rounded-t-2xl">
                 <div class="flex flex-col w-full gap-4 p-4 bg-white rounded-b-2xl">
                     <div class="flex flex-col justify-start gap-2">
@@ -41,7 +41,31 @@
                     <span class="text-sm text-gray-400">Outwear</span>
                 </div>
             </div>
-            <div class="flex flex-col w-1/4 bg-white rounded-2xl">
+            <div class="flex flex-col w-[calc(100%/6)] bg-white rounded-2xl">
+                <img class="w-full bg-gray-300 h-60 rounded-t-2xl">
+                <div class="flex flex-col w-full gap-4 p-4 bg-white rounded-b-2xl">
+                    <div class="flex flex-col justify-start gap-2">
+                        <span class="text-base font-medium">Cardigan</span>
+                        <div class="flex flex-col gap-1">
+                            <span class="text-xl font-bold text-primary">Rp 45.000</span>
+                        </div>
+                    </div>
+                    <span class="text-sm text-gray-400">Outwear</span>
+                </div>
+            </div>
+            <div class="flex flex-col w-[calc(100%/6)] bg-white rounded-2xl">
+                <img class="w-full bg-gray-300 h-60 rounded-t-2xl">
+                <div class="flex flex-col w-full gap-4 p-4 bg-white rounded-b-2xl">
+                    <div class="flex flex-col justify-start gap-2">
+                        <span class="text-base font-medium">Cardigan</span>
+                        <div class="flex flex-col gap-1">
+                            <span class="text-xl font-bold text-primary">Rp 45.000</span>
+                        </div>
+                    </div>
+                    <span class="text-sm text-gray-400">Outwear</span>
+                </div>
+            </div>
+            <div class="flex flex-col w-[calc(100%/6)] bg-white rounded-2xl">
                 <img class="w-full bg-gray-300 h-60 rounded-t-2xl">
                 <div class="flex flex-col w-full gap-4 p-4 bg-white rounded-b-2xl">
                     <div class="flex flex-col justify-start gap-2">
@@ -55,7 +79,7 @@
             </div>
     </div>
     <div class="flex justify-start w-full gap-4">
-        <div class="flex flex-col w-1/4 bg-white rounded-2xl">
+        <div class="flex flex-col w-[calc(100%/6)] bg-white rounded-2xl">
             <img class="w-full bg-gray-300 h-60 rounded-t-2xl">
             <div class="flex flex-col w-full gap-4 p-4 bg-white rounded-b-2xl">
                 <div class="flex flex-col justify-start gap-2">
@@ -67,7 +91,7 @@
                 <span class="text-sm text-gray-400">Outwear</span>
             </div>
         </div>
-        <div class="flex flex-col w-1/4 bg-white rounded-2xl">
+        <div class="flex flex-col w-[calc(100%/6)] bg-white rounded-2xl">
             <img class="w-full bg-gray-300 h-60 rounded-t-2xl">
             <div class="flex flex-col w-full gap-4 p-4 bg-white rounded-b-2xl">
                 <div class="flex flex-col justify-start gap-2">
@@ -79,7 +103,7 @@
                 <span class="text-sm text-gray-400">Outwear</span>
             </div>
         </div>
-        <div class="flex flex-col w-1/4 bg-white rounded-2xl">
+        <div class="flex flex-col w-[calc(100%/6)] bg-white rounded-2xl">
             <img class="w-full bg-gray-300 h-60 rounded-t-2xl">
             <div class="flex flex-col w-full gap-4 p-4 bg-white rounded-b-2xl">
                 <div class="flex flex-col justify-start gap-2">
@@ -91,7 +115,7 @@
                 <span class="text-sm text-gray-400">Outwear</span>
             </div>
         </div>
-        <div class="flex flex-col w-1/4 bg-white rounded-2xl">
+        <div class="flex flex-col w-[calc(100%/6)] bg-white rounded-2xl">
             <img class="w-full bg-gray-300 h-60 rounded-t-2xl">
             <div class="flex flex-col w-full gap-4 p-4 bg-white rounded-b-2xl">
                 <div class="flex flex-col justify-start gap-2">
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,10 +3,10 @@
 @section('page')
     <div class="flex justify-center my-10">
         <div class="flex max-w-[1440px] w-full flex-col gap-10">
-            @include('components.banner')
-            @include('components.flash-sale')
-            @include('components.category')
-            @include('components.new-product')
+            <x-layout.banner/>
+            <x-layout.flash-sale/>
+            <x-layout.category/>
+            <x-layout.new-product/>
         </div>
     </div>
 @endsection
