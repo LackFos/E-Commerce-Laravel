840055f9689de157109bb6b0c9dd1e11d8213a37
crendyyy <crendy.realdi12@gmail.com>
2023-12-19 08:28:09 +0700

    fix some

--- routes/web.php
+++ routes/web.php
@@ -40,18 +40,6 @@ Route::prefix('cart')->group(function () {
     });
 });
 
-// Route::get('/cart', function () {
-//     return view('pages.cart');
-// });
-
-Route::prefix('cart')->group(function () {
-    Route::controller(CartController::class)->group(function () {
-        Route::get('/', 'getCartItems');
-        Route::post('/add', 'addToCart');
-        Route::post('/remove', 'addToCart');
-    });
-});
-
 Route::get('/demodashboard/{section?}/{action?}', function ($section = null, $action = null) {
     $view = $section ? 'pages.dashboard-' . $section : 'pages.dashboard';
     if ($action) {
