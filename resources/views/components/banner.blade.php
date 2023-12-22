150a7a261b287382b6ae408212d283c9170482b7
LackFos <80957646+LackFos@users.noreply.github.com>
2023-12-22 10:53:23 +0700

    Fix Merge Bugs

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <div class="swiper-banner relative overflow-hidden rounded-lg">
     <div class="swiper-wrapper flex gap-4">
         @foreach ($banners as $banner)
@@ -6,22 +5,6 @@
                 <img class="h-80 w-full rounded-lg bg-[#f0f0f0] object-cover" src="{{ $banner->image }}" alt="{{ $banner->name }}">
             </div>
         @endforeach
-=======
-<div class="relative overflow-hidden rounded-lg swiper-banner">
-    <div class="flex gap-4 swiper-wrapper">
-        <div class="swiper-slide">
-            <img class="h-80 w-full rounded-lg bg-[#f0f0f0]" src="" alt="afha">
-        </div>
-        <div class="swiper-slide">
-            <img class="h-80 w-full rounded-lg bg-[#f0f0f0]" src="" alt="afha">
-        </div>
-        <div class="swiper-slide">
-            <img class="h-80 w-full rounded-lg bg-[#f0f0f0]" src="" alt="afha">
-        </div>
-        <div class="swiper-slide">
-            <img class="h-80 w-full rounded-lg bg-[#f0f0f0]" src="" alt="afha">
-        </div>
->>>>>>> Stashed changes
     </div>
     <div class="swiper-pagination"></div>
 </div>
