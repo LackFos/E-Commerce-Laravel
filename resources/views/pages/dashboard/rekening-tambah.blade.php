695eaff4c8f0dbbd3f2af5692495af7e473766f4
crendyyy <crendy.realdi12@gmail.com>
2023-12-25 19:20:56 +0700

    fix layout rekening

--- resources/views/pages/dashboard/rekening-tambah.blade.php
+++ resources/views/pages/dashboard/rekening-tambah.blade.php
@@ -2,41 +2,43 @@
 
 @section('page')
     <div class="flex gap-2">
-        <div class="flex w-full max-w-[1440px] justify-center">
+        <div class="flex w-full max-w-[1440px]">
             <x-layout.sidebar />
-            <form method="POST" action="{{ route('paymentaccount.store') }}" class="flex w-full flex-col gap-6 p-10">
-                @csrf
-                <div class="flex w-full justify-start gap-2">
-                    <span class="font-semibold"><a href="/dashboard/rekening" class="text-black">Rekening</a></span>
-                    <span>•</span>
+            <x-layout.content>
+                <form method="POST" action="{{ route('paymentaccount.store') }}" class="flex flex-col w-full gap-6 p-10">
+                    @csrf
+                    <div class="flex justify-start w-full gap-2">
+                        <span class="font-semibold"><a href="/dashboard/rekening" class="text-black">Rekening</a></span>
+                        <span>•</span>
                     <span aria-current="page" class="text-gray-500 active:font-semibold">Tambah Rekening</span>
                 </div>
-                <div class="flex w-full flex-col rounded-lg border border-solid border-gray-200 bg-white">
-                    <span class="flex justify-start rounded-t-lg border-b border-solid border-gray-200 bg-white p-6 text-lg font-bold">Tambah Rekening</span>
+                <div class="flex flex-col w-full bg-white border border-gray-200 border-solid rounded-lg">
+                    <span class="flex justify-start p-6 text-lg font-bold bg-white border-b border-gray-200 border-solid rounded-t-lg">Tambah Rekening</span>
                     <div class="flex gap-6 px-6 py-2">
-                        <div class="flex w-1/2 flex-col justify-start gap-1">
+                        <div class="flex flex-col justify-start w-1/2 gap-1">
                             <span class="flex font-medium">Nama Penerima:</span>
                         </div>
-                        <input placeholder="Masukan Nama Penerima" class="w-1/2 rounded-lg border border-solid border-gray-200 px-4 py-1" type="text" name="bank_owner"
-                            name="bank_owner" value="{{ old('bank_owner') }}">
+                        <input placeholder="Masukan Nama Penerima" class="w-1/2 px-4 py-1 border border-gray-200 border-solid rounded-lg" type="text" name="bank_owner"
+                        name="bank_owner" value="{{ old('bank_owner') }}">
                     </div>
                     <div class="flex gap-6 px-6 py-2">
-                        <div class="flex w-1/2 flex-col justify-start gap-1">
+                        <div class="flex flex-col justify-start w-1/2 gap-1">
                             <span class="font-medium leading-8">Nama Bank:</span>
                         </div>
-                        <input placeholder="Masukan Nama Bank" class="w-1/2 rounded-lg border border-solid border-gray-200 px-4 py-1" type="text" name="bank_name"
-                            value="{{ old('bank_name') }}">
+                        <input placeholder="Masukan Nama Bank" class="w-1/2 px-4 py-1 border border-gray-200 border-solid rounded-lg" type="text" name="bank_name"
+                        value="{{ old('bank_name') }}">
                     </div>
                     <div class="flex gap-6 px-6 py-2">
-                        <div class="flex w-1/2 flex-col justify-start gap-1">
+                        <div class="flex flex-col justify-start w-1/2 gap-1">
                             <span class="font-medium leading-8">No Rekening:</span>
                         </div>
-                        <input placeholder="Masukan No Rekening" class="w-1/2 rounded-lg border border-solid border-gray-200 px-4 py-1" type="texxt" name="bank_number"
-                            value="{{ old('bank_number') }}">
+                        <input placeholder="Masukan No Rekening" class="w-1/2 px-4 py-1 border border-gray-200 border-solid rounded-lg" type="texxt" name="bank_number"
+                        value="{{ old('bank_number') }}">
                     </div>
                 </div>
-                <button type="submit" class="w-full rounded-lg bg-primary px-6 py-2 text-center font-semibold leading-8 text-white">Simpan</button>
+                <button type="submit" class="w-full px-6 py-2 font-semibold leading-8 text-center text-white rounded-lg bg-primary">Simpan</button>
             </form>
+        </x-layout.content>
         </div>
     </div>
-@endsection
+    @endsection
--- resources/views/pages/dashboard/rekening.blade.php
+++ resources/views/pages/dashboard/rekening.blade.php
@@ -2,42 +2,43 @@
 
 @section('page')
     <div class="flex gap-2">
-        <div class="flex w-full max-w-[1440px] justify-center">
+        <div class="flex w-full max-w-[1440px]">
             <x-layout.sidebar />
-            <div class="flex w-full flex-col gap-6 p-10">
-                <h2>Rekening</h2>
-
-                <div class="flex w-full flex-col rounded-lg border border-solid border-gray-200 bg-white">
-                    <div class='flex min-h-[64px] items-center gap-6 px-6'>
+            <x-layout.content>
+                <div class="flex flex-col w-full gap-6 p-10">
+                    <h2>Rekening</h2>
+                    
+                    <div class="flex flex-col w-full bg-white border border-gray-200 border-solid rounded-lg">
+                        <div class='flex min-h-[64px] items-center gap-6 px-6'>
                         <div class='flex h-full min-h-[64px] w-full'>
-                            <div class='g-4 flex w-full flex-col items-start justify-center font-bold'>Nama</div>
-                            <div class='g-4 flex w-full flex-col items-start justify-center font-bold'>Bank</div>
-                            <div class='g-4 flex w-full flex-col items-start justify-center font-bold'>Nomor Rekening</div>
-                            <div class='g-4 flex w-full flex-col items-start justify-center font-bold'>Aksi</div>
+                            <div class='flex flex-col items-start justify-center w-full font-bold g-4'>Nama</div>
+                            <div class='flex flex-col items-start justify-center w-full font-bold g-4'>Bank</div>
+                            <div class='flex flex-col items-start justify-center w-full font-bold g-4'>Nomor Rekening</div>
+                            <div class='flex flex-col items-start justify-center w-full font-bold g-4'>Aksi</div>
                         </div>
                     </div>
 
                     @foreach ($paymentAccounts as $paymentAccount)
-                        <form action="{{ route('paymentaccount.destroy', ['paymentAccount' => $paymentAccount->id]) }}" method="POST"
-                            class='flex min-h-[64px] items-center gap-6 border-t border-solid border-gray-200 px-6'>
+                    <form action="{{ route('paymentaccount.destroy', ['paymentAccount' => $paymentAccount->id]) }}" method="POST"
+                        class='flex min-h-[64px] items-center gap-6 border-t border-solid border-gray-200 px-6'>
                             @csrf
                             @method('DELETE')
 
                             <div class='flex h-full min-h-[64px] w-full'>
-                                <div class='flex w-full flex-col items-start justify-center font-light text-gray-500'>
-                                    <span class="w-fit rounded-lg text-gray-600">{{ $paymentAccount->bank_owner }}</span>
+                                <div class='flex flex-col items-start justify-center w-full font-light text-gray-500'>
+                                    <span class="text-gray-600 rounded-lg w-fit">{{ $paymentAccount->bank_owner }}</span>
                                 </div>
-                                <div class='flex w-full flex-col items-start justify-center font-light text-gray-500'>
-                                    <span class="w-fit rounded-lg text-gray-600">{{ $paymentAccount->bank_name }}</span>
+                                <div class='flex flex-col items-start justify-center w-full font-light text-gray-500'>
+                                    <span class="text-gray-600 rounded-lg w-fit">{{ $paymentAccount->bank_name }}</span>
                                 </div>
-                                <div class='flex w-full flex-col items-start justify-center font-light text-gray-500'>
+                                <div class='flex flex-col items-start justify-center w-full font-light text-gray-500'>
                                     <div class="flex gap-4">
-                                        <span class="w-fit rounded-lg text-gray-600">{{ $paymentAccount->bank_number }}</span>
+                                        <span class="text-gray-600 rounded-lg w-fit">{{ $paymentAccount->bank_number }}</span>
                                     </div>
                                 </div>
-                                <div class='flex w-full flex-col items-start justify-center'>
-                                    <div class='flex w-full items-center justify-start gap-4'>
-                                        <button class="rounded-lg bg-primary px-2 py-2 hover:bg-opacity-80" type="submit"><x-icons.trash /></button>
+                                <div class='flex flex-col items-start justify-center w-full'>
+                                    <div class='flex items-center justify-start w-full gap-4'>
+                                        <button class="px-2 py-2 rounded-lg bg-primary hover:bg-opacity-80" type="submit"><x-icons.trash /></button>
                                     </div>
                                 </div>
                             </div>
@@ -49,6 +50,7 @@
                     </div>
                 </div>
             </div>
+        </x-layout.content>
         </div>
     </div>
 @endsection
