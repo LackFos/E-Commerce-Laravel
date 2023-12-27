139d9cf962266305c06a1dd89641b8e35e41ecab
William Wijaya <LackFosisOnline@gmail.com>
2023-12-27 15:47:46 +0700

    Hide box if flashsale is empty

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,10 +2,12 @@
 
 @section('page')
     <x-layout.user>
-        <div class="flex justify-center my-10">
-            <div class="flex w-full max-w-[1440px] px-8 flex-col gap-10">
+        <div class="my-10 flex justify-center">
+            <div class="flex w-full max-w-[1440px] flex-col gap-10 px-8">
                 <x-banner :banners="$banners" />
-                <x-flash-sale :flashsale="$flashsale" />
+                @if (count($flashsale) > 0)
+                    <x-flash-sale :flashsale="$flashsale" />
+                @endif
                 <x-categories :categories="$categories" />
                 <x-new-product :products="$latestProducts" />
             </div>
